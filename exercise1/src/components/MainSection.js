9cf06fc42710b9d6d6429732affbf6722150758e
Saana Pullola <t0pusa00@students.oamk.fi>
2021-09-25 19:59:44 +0300

    exercise1

--- exercise1/src/components/MainSection.js
+++ exercise1/src/components/MainSection.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import styles from './MainSection.module.css'
-import img1 from '../images/image1.png'
 
 
-export default function MainSection() {
+export default function MainSection(props) {
     return (
-
+   
         <div className = {styles.container}>
             <div>
-            <img src = {img1} alt = ""/>
-            <h1 style = {{marginTop: '10px'}}> <span style = {{color: 'rgb(0, 81, 141)'}}> Petokset | </span> Poliisi: Omakanta palvelulta näyttävät valesivustot
-            kalastelevat pankkitunnuksia - näin suojaudut huijaukselta </h1>
+          
+            <img src = {props.img} alt = ""/>
+            <h1 style = {{marginTop: '10px'}}> <span style = {{color: 'rgb(0, 81, 141)'}}> {props.topic} | </span> {props.body} </h1>
             </div>
-        </div>
+            </div>
         
     )
 }
