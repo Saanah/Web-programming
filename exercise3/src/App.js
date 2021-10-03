a69674b1a29dbe418a5a30774aef3b0de0936792
Saana Pullola <t0pusa00@students.oamk.fi>
2021-10-03 23:03:39 +0300

    exercise 3 search bar event

--- exercise3/src/App.js
+++ exercise3/src/App.js
@@ -8,16 +8,23 @@ class App extends React.Component {
 constructor(props)
 {
   super(props)
-  this.state =  { items: data.items  
+  this.state =  { items: data.items,  //Saves data from the json file in an array
+    itemSearchString: ""              //String that is modified according to the given value on the search bar
 }
+
+}
+
+onSearchChange = (event) => {                              //handles the event when something is typed on the search bar
+this.setState({itemSearchString: event.target.value})     //puts the given value into the string
 }
 
 render()
 {
 return (
   <div className="App">
-  <Header/>
-  <SearchComputerItems items={this.state.items}/>
+  <Header onSearchChange = {this.onSearchChange} itemSearchString = {this.state.itemSearchString}/>
+  <SearchComputerItems items={this.state.items.filter(           //filters items based on the string value and sends them as props
+    (item) => item.name.includes(this.state.itemSearchString))}/>  
   </div>
 );
 }
